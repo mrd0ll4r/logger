fe4c25a592ab5ca9814ac191ced06e571ca9b74f
Leo Balduf <balduf@hm.edu>
2015-11-14 17:36:00 +0100

    renamed test files

--- logger_test.go
+++ logger_test.go
@@ -1,6 +1,3 @@
-// Copyright (C) 2014 Jakob Borg. All rights reserved. Use of this source code
-// is governed by an MIT-style license that can be found in the LICENSE file.
-
 package logger
 
 import (
@@ -17,47 +14,4 @@ func TestLevels(t *testing.T) {
 	l.Infoln("hello info")
 	l.Okln("hello OK")
 	l.Warnln("hello warn")
-}
-
-// All benchmarks should be ran with LOGGER_DISCARD="<non-empty>", to redirect logging to ioutil.Discard
-
-func BenchmarkLshortfileLn(b *testing.B) {
-	l := NewStdlibLogger()
-	l.SetFlags(log.Lshortfile)
-	l.SetLevel(LevelOK)
-	s := "hi"
-	for i := 0; i < b.N; i++ {
-		l.Okln(s)
-	}
-}
-
-func BenchmarkNoFlagLn(b *testing.B) {
-	l := NewStdlibLogger()
-	l.SetFlags(0)
-	l.SetLevel(LevelOK)
-	s := "hi"
-	for i := 0; i < b.N; i++ {
-		l.Okln(s)
-	}
-}
-
-func BenchmarkNoFlagF(b *testing.B) {
-	l := NewStdlibLogger()
-	l.SetFlags(0)
-	l.SetLevel(LevelOK)
-	s := ""
-	s1 := "hi"
-	for i := 0; i < b.N; i++ {
-		l.Okf(s, s1)
-	}
-}
-
-func BenchmarkLevelTooLow(b *testing.B) {
-	l := NewStdlibLogger()
-	l.SetFlags(0)
-	l.SetLevel(LevelOK)
-	s := "hi"
-	for i := 0; i < b.N; i++ {
-		l.Debugln(s)
-	}
 }
---
+++ stdlibLogger_test.go
@@ -0,0 +1,52 @@
+// Copyright (C) 2014 Jakob Borg. All rights reserved. Use of this source code
+// is governed by an MIT-style license that can be found in the LICENSE file.
+
+package logger
+
+import (
+	"log"
+	"testing"
+)
+
+// All benchmarks should be ran with LOGGER_DISCARD="<non-empty>", to redirect logging to ioutil.Discard
+
+func BenchmarkLshortfileLn(b *testing.B) {
+	l := NewStdlibLogger()
+	l.SetFlags(log.Lshortfile)
+	l.SetLevel(LevelOK)
+	s := "hi"
+	for i := 0; i < b.N; i++ {
+		l.Okln(s)
+	}
+}
+
+func BenchmarkNoFlagLn(b *testing.B) {
+	l := NewStdlibLogger()
+	l.SetFlags(0)
+	l.SetLevel(LevelOK)
+	s := "hi"
+	for i := 0; i < b.N; i++ {
+		l.Okln(s)
+	}
+}
+
+func BenchmarkNoFlagF(b *testing.B) {
+	l := NewStdlibLogger()
+	l.SetFlags(0)
+	l.SetLevel(LevelOK)
+	s := ""
+	s1 := "hi"
+	for i := 0; i < b.N; i++ {
+		l.Okf(s, s1)
+	}
+}
+
+func BenchmarkLevelTooLow(b *testing.B) {
+	l := NewStdlibLogger()
+	l.SetFlags(0)
+	l.SetLevel(LevelOK)
+	s := "hi"
+	for i := 0; i < b.N; i++ {
+		l.Debugln(s)
+	}
+}
