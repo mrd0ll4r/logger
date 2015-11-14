23b63d9e8503b4f761b1ab4a83924299195bfede
Leo Balduf <balduf@hm.edu>
2015-11-14 14:11:26 +0100

    manual level testing now actually shows all levels

--- logger_test.go
+++ logger_test.go
@@ -11,6 +11,7 @@ import (
 func TestLevels(t *testing.T) {
 	l := NewStdlibLogger()
 	l.SetFlags(log.Lshortfile | log.Ldate | log.Ltime)
+	l.SetLevel(Everything)
 	l.Debugln("hello debug")
 	l.Verboseln("hello verbose")
 	l.Infoln("hello info")
